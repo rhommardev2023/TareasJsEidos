988fc13529620529c42982340bba759670af3594
rhommardev2023 <rjmlp02@gmail.com>
2023-07-09 17:30:10 -0300

    desafios 5 y 6

---
+++ tarea 5-js/clases-js/js/main.js
@@ -0,0 +1,13 @@
+console.log('Hola Mundo!')
+
+let num1 = 50;
+let num2 = 10;
+let saludo = '¡Hola!';
+let nombreCurso = 'Código LATAM';
+let resultadoSuma = num1 + num2;
+let resultadoResta = num1 - num2;
+let resultadoMultiplicacion = num1 * num2;
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion);
+console.log(saludo + 'Esto es ' + nombreCurso);
---
+++ tarea6Js/Js/tarea6Js.js
@@ -0,0 +1,8 @@
+function cuentaRegresiva(numero) {
+
+    for (let i = numero;  i >= 0;  i-- ) {
+    console.log (i);
+}
+    console.log("¡Despegamos!")
+}
+    cuentaRegresiva (10)
